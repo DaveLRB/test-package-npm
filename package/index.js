b2d8f8c6b6fc455d972162cba1b10afe364a5269
David Barrela <davleoribeiro@hotmail.com>
2024-06-27 11:01:29 +0100

    package published correctly

---
+++ package/index.js
@@ -0,0 +1,7 @@
+//test-package/package/index.js
+
+function test(string) {
+    return string === 'hello world';
+};
+
+export default test;
---
+++ test-folder/script.js
@@ -0,0 +1,7 @@
+//test-package/test-folder/script.js
+
+import test from "../package/index.js";
+
+const test1 = test;
+
+console.log(test('hello world'));
